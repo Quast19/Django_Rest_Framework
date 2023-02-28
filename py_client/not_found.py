42b5e812625da5e116f0d3fabae92ed40b52bf85
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2023-02-28 11:24:07 +0530

    changes in py_client

--- py_client/not_found.py
+++ py_client/not_found.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://localhost:8000/api/products/1/"
+endpoint = "http://localhost:8000/api/products/99999999999/"
 
 res = requests.get(endpoint) # Application programming interface 
 #print(res.headers)
